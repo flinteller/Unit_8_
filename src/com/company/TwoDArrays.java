9e04f25b57129ed041f965eb03291b0e6d596d5f
flinteller <flint.eller@ssfs.org>
2020-03-19 12:10:17 -0400

    Finished isSquare method

--- src/com/company/TwoDArrays.java
+++ src/com/company/TwoDArrays.java
@@ -29,12 +29,29 @@ public class TwoDArrays {
         return total;
     }
 
+    public static boolean isSquare(int[][] arr){
+        boolean sqr = false;
+
+        if(arr.length == arr[0].length){
+            sqr = true;
+        }
+
+        return sqr;
+    }
+
+
     public static void main(String args[]){
         int[][] list = {{21, -4, 89}, {65, 33, 1}, {-2, 3, 1}};
         System.out.println(maxValue(list));
 
         int[][] nums = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         System.out.println(sumAll(nums));
+
+        int[][] arr1 = {{1, 2, 3, 4}, {6, 2, 5, 3}, {9, 0, 1, 7}, {3, 1, 8, 6}};
+        int[][] arr2 = {{1, 6, 4}, {6, 2, 1}};
+        System.out.println(isSquare(arr1));
+        System.out.println(isSquare(arr2));
+
 
     }
 
